9f7cd9ef6e3000eacf4f0b12e6e341e31d74de94
sayo <sayomayomaki@gmail.com>
2026-01-13 11:03:39 +0000

    Add AoC day2 part 1 processing

--- src/aoc.ml
+++ src/aoc.ml
@@ -8,40 +8,90 @@ module I = struct
   type 'a t =
     { clock : 'a
     ; reset : 'a
-    ; data : 'a [@bits 32]
-    ; data_ready : 'a
-    ; completed : 'a
+    ; data : 'a With_valid.t [@bits 32]
+    ; puzzle : 'a [@bits 5] (* 4 bit for day, 1 bit for part 1/2 *)
     }
   [@@deriving hardcaml]
 end
 
 module O = struct
   type 'a t = 
-    {
-      result : 'a With_valid.t [@bits 8]
+    { result : 'a With_valid.t [@bits 32] 
+    ; data_ready : 'a 
     }
   [@@deriving hardcaml]
 end
 
-let create scope ({ clock; reset; data; data_ready; completed } : _ I.t) : _ O.t
+let create scope ({ clock; reset; data; puzzle} : _ I.t) : _ O.t
   = 
   let open Always in
   let spec = Reg_spec.create ~clock ~clear:reset () in
-  let _data = data in
-  let _ready = data_ready in
-  let _completed = completed in
-  let _scope = scope in
 
-  let result = Variable.reg spec ~width:8 in
-  let result_valid = Variable.wire ~default:vdd () in
+  let result = Variable.reg spec ~width:32 in
+  let result_valid = Variable.reg spec ~width:1 in
+  let data_ready = Variable.wire ~default:gnd () in
+
+  let day2 enable =
+    let convert = Variable.reg spec ~width:1 in
+    let convert_started = Variable.reg spec ~width:1 in
+    let increment = Variable.reg spec ~width:1 in 
+    let bcd_reset = ~:enable in
+
+    let start_value = Variable.reg spec ~width:32 in
+    let end_value = Variable.reg spec ~width:32 in
+    let%hw_var range = Variable.reg spec ~width:32 in
+    let%hw_var range_counter = Variable.reg spec ~width:32 in
+    let%hw_var invalid_counter = Variable.reg spec ~width:32 in
+
+    let bcd = Bcd.hierarchical scope in
+    let number = bcd { Bcd.I.clock = clock; reset = bcd_reset; convert = convert.value; start_value = start_value.value; increment = increment.value } in
+
+    (* todo *)
+    [
+      if_ (start_value.value ==:. 0) [
+        data_ready <-- vdd;
+
+        when_ (data.valid) [
+          start_value <-- data.value;
+        ];
+      ] (elif (end_value.value ==:. 0) [
+        data_ready <-- vdd;
+
+        when_ (data.valid) [
+          end_value <-- data.value;
+        ];
+      ] [
+        range <-- (end_value.value -: start_value.value) +:. 1;
+        convert <-- gnd;
+        increment <-- gnd;
+
+        when_ ~:(convert_started.value) [
+          convert <-- vdd;
+          convert_started <-- vdd;
+        ];
+
+        when_ (convert_started.value ==: vdd) [
+          if_ (range_counter.value <=: range.value) [
+            when_ ((number.ready ==: vdd) &: (increment.value ==: gnd)) [
+              invalid_counter <-- invalid_counter.value +: (uresize number.is_invalid ~width:32);
+              increment <-- vdd;
+              range_counter <-- range_counter.value +:. 1;
+            ];
+          ] [
+            result <-- invalid_counter.value;
+            result_valid <-- vdd;
+          ];
+        ];
+      ]);
+    ]
+  in
 
   compile 
     [
-      result <-- result.value +:. 1;
-      result_valid <-- vdd;
+      when_ (puzzle ==:. 0b100) (day2 vdd) (* day 2 part 1 *)
     ]; 
 
-  { result = { value = result.value; valid = result_valid.value } }
+  { result = { value = result.value; valid = result_valid.value }; data_ready = data_ready.value }
 ;;
 
 let hierarchical scope = 
--- src/aoc.mli
+++ src/aoc.mli
@@ -7,16 +7,18 @@ module I : sig
   type 'a t = 
     { clock : 'a
     ; reset : 'a
-    ; data : 'a
-    ; data_ready : 'a
-    ; completed : 'a
+    ; data : 'a With_valid.t
+    ; puzzle : 'a
     }
   [@@deriving hardcaml]
 end
 
 module O : sig 
-  type 'a t = { result : 'a With_valid.t } [@@deriving hardcaml]
+  type 'a t = 
+    { result : 'a With_valid.t
+    ; data_ready : 'a
+    } 
+  [@@deriving hardcaml]
 end
 
-val create : Scope.t -> Signal.t I.t -> Signal.t O.t
 val hierarchical : Scope.t -> Signal.t I.t -> Signal.t O.t
